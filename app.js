ad7dc6ad8d2ccfc716dff596d1b8c390e3810a9c
Darpit Makwana <darpitpatel2008@gmail.com>
2022-01-18 17:59:55 +0530

    updated

---
+++ app.js
@@ -0,0 +1,6 @@
+const checkInBtn = document.getElementById("checkInBtn");
+const checkOutBtn = document.getElementById("checkOutBtn");
+const breakInBtn = document.getElementById("breakInBtn");
+const breakOutBtn = document.getElementById("breakOutBtn");
+
+console.log(checkInBtn,checkOutBtn,breakInBtn,breakOutBtn)
